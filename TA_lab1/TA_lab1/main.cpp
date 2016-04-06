ca07374048bc2e3b64eb8a5cc304ddf237be7a33
Nikita Kuzin <nikitakyza@gmail.com>
2016-04-06 17:02:59 +0300

    порефакторил код для читаемости

--- TA_lab1/TA_lab1/main.cpp
+++ TA_lab1/TA_lab1/main.cpp
@@ -13,18 +13,17 @@ static const string TRANSITIONS_TYPE = "transitions";
 static const string STATES_TYPE = "states";
 
 
-Array GetTypeArray(Object const &obj, string const &typeObject)
+Array GetStatesArray(Object const &stateMachine, string const& key)
 {
 	Array statesArray;
 
-	for (size_t i = 0; i < obj.size(); ++i)
+	for (size_t i = 0; i < stateMachine.size(); ++i)
 	{
-		if (obj[i].name_ == typeObject)
+		if (stateMachine[i].name_ == key)
 		{
-			return obj[i].value_.get_array();
+			return stateMachine[i].value_.get_array();
 		}
 	}
-	
 	return{};
 }
 
@@ -32,14 +31,14 @@ vector<Pair>* ParseMeale(Object const &object)
 {
 	vector<string > statesArray;
 
-	auto states = GetTypeArray(object, STATES_TYPE);
+	auto states = GetStatesArray(object, STATES_TYPE);
 
 	for (auto state : states)
 	{
 		statesArray.push_back(state.get_obj().at(0).value_.get_str()); //вектор состояние q1...q3
 	}
 
-	auto transitions = GetTypeArray(object, TRANSITIONS_TYPE);
+	auto transitions = GetStatesArray(object, TRANSITIONS_TYPE);
 	for (auto transition : transitions)
 	{
 		cout << endl;
@@ -59,15 +58,17 @@ vector<Pair>* GetAutomats(string const &file_name)
 	Value value;
 	read(inputFile, value);
 
-	auto automats = value.get_obj().at(0).value_.get_array();
+	auto stateMachinesArray = value.get_obj().at(0).value_.get_array();
+	auto firstSM = stateMachinesArray.at(0).get_obj();
+	auto seconSM = stateMachinesArray.at(1).get_obj();
+	auto firstType = firstSM.at(1).value_.get_str();
+	auto seconType = seconSM.at(1).value_.get_str();
 
-	auto type = automats.at(0).get_obj().at(1).value_.get_str();
+	cout << firstType << endl;
 
-	cout << type << endl;
-
-	if (type == MEALE_TYPE)
+	if (firstType == MEALE_TYPE)
 	{
-		ParseMeale(automats.at(0).get_obj());
+		ParseMeale(firstSM);
 	}
 
 	return{};

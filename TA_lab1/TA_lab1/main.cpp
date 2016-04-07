db7d1f8c792285ac4ef0a647f9d79b3cc66de81e
Nikita Kuzin <nikitakyza@gmail.com>
2016-04-07 12:21:37 +0300

    Реализовал парсинг Мура

--- TA_lab1/TA_lab1/main.cpp
+++ TA_lab1/TA_lab1/main.cpp
@@ -1,3 +1,4 @@
+#include "stdafx.h"
 #include <vector>
 #include <string>
 #include "json_spirit/json_spirit.h"
@@ -9,11 +10,13 @@ using namespace std;
 using namespace json_spirit;
 
 static const string MEALE_TYPE = "meale";
+static const string MOORE_TYPE = "moore";
 static const string TRANSITIONS_TYPE = "transitions";
 static const string STATES_TYPE = "states";
+static const pair<string, string> EMPTY_CELL = { "", "" };
 
 
-Array GetStatesArray(Object const &stateMachine, string const& key)
+Array GetArray(Object const &stateMachine, string const& key)
 {
 	Array statesArray;
 
@@ -31,14 +34,14 @@ vector<Pair>* ParseMeale(Object const &object)
 {
 	vector<string > statesArray;
 
-	auto states = GetStatesArray(object, STATES_TYPE);
+	auto states = GetArray(object, STATES_TYPE);
 
 	for (auto state : states)
 	{
 		statesArray.push_back(state.get_obj().at(0).value_.get_str()); //вектор состояние q1...q3
 	}
 
-	auto transitions = GetStatesArray(object, TRANSITIONS_TYPE);
+	auto transitions = GetArray(object, TRANSITIONS_TYPE);
 	for (auto transition : transitions)
 	{
 		cout << endl;
@@ -49,6 +52,68 @@ vector<Pair>* ParseMeale(Object const &object)
 	}
 
 	return {};
+}
+
+map<string, string> GetEmptyMap(size_t size)
+{
+	map<string, string> map;
+	for (size_t i = 0; i < size; i++)
+	{
+		map.emplace(EMPTY_CELL);
+	}
+	return map;
+}
+
+
+vector<vector<pair<string, string>>> GetMooreTable(Object const &object)
+{
+	auto table = vector<vector<pair<string, string>>>(1);
+	table[0].push_back(EMPTY_CELL);
+	vector<string> inputs;
+
+	auto states = GetArray(object, STATES_TYPE);
+
+	for (auto & state : states)
+	{
+		pair<string, string> cellState;
+		cellState.first = state.get_obj()[0].value_.get_str();
+		cellState.second = state.get_obj()[1].value_.get_str();
+		table[0].push_back(cellState);
+	}
+	auto columnsCount = table[0].size();
+
+	auto transitions = GetArray(object, TRANSITIONS_TYPE);
+
+	for (auto & transition : transitions)
+	{
+		auto input = transition.get_obj()[0].value_.get_str();
+		auto from = transition.get_obj()[1].value_.get_str();
+		auto to = transition.get_obj()[2].value_.get_str();
+		auto iterator = std::find(inputs.begin(), inputs.end(), input);
+		auto iter = find_if(table[0].begin(), table[0].end(), [&](auto const& pair)
+		{
+			return pair.first == from;
+		});
+		auto columnPos = iter - table[0].begin();
+		if (iterator == inputs.end())
+		{
+			auto vec = vector<pair<string, string>>(columnsCount);
+			vec[0] = { input, "" };
+			vec[columnPos] = {to, ""};
+			table.push_back(vec);
+			inputs.push_back(input);
+		}
+		else
+		{
+			auto rowWithInput = find_if(table.begin(), table.end(), [&](auto const& row)
+			{
+				return row[0].first == input;
+			});
+			rowWithInput->at(columnPos) = { to, "" };
+		}
+	}
+
+	return table;
 }
 
 vector<Pair>* GetAutomats(string const &file_name)
@@ -65,7 +130,7 @@ vector<Pair>* GetAutomats(string const &file_name)
 	auto seconType = seconSM.at(1).value_.get_str();
 
 	cout << firstType << endl;
-
+	GetMooreTable(seconSM);
 	if (firstType == MEALE_TYPE)
 	{
 		ParseMeale(firstSM);
